07df53cc582aabb4b2ca991dd3902740ad58265a
Shafin98 <98sasiam@gmail.com>
2026-04-08 17:50:02 +0600

    fixed url issue

--- dev/dev/urls.py
+++ dev/dev/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('devapp.urls')),
     path('backend/', include('devapp.urls')),
 ]
 
