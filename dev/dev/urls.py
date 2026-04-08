5ea1582f24c4a86462bf18495bb9b469baaa3334
Shafin98 <98sasiam@gmail.com>
2026-04-08 18:05:14 +0600

    Fix URL + static config crash

--- dev/dev/urls.py
+++ dev/dev/urls.py
@@ -22,9 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('devapp.urls')),
-    path('backend/', include('devapp.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
