c97a2080c3eb6c328f59448aad5a4b2177eb5876
Shafin98 <98sasiam@gmail.com>
2026-04-16 22:08:43 +0600

    feat: added postgrade db for render

--- dev/dev/settings.py
+++ dev/dev/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -107,13 +108,20 @@ WSGI_APPLICATION = 'dev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# Dev DB
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',  # fallback for local dev
+        conn_max_age=600
+    )
 }
+
+# Dev DB
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Production DB
 # DATABASES = {
