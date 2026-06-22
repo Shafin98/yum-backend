bbd620ee850275d8be96561a942cde3c0cc2d87b
Shafin98 <98sasiam@gmail.com>
2026-06-22 18:58:28 +0600

    fix: changed the name Service from Category on admin.py

--- dev/devapp/admin.py
+++ dev/devapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Service)
 admin.site.register(Logo)
 admin.site.register(SocialMediaAccounts)
 admin.site.register(Testimonial)
